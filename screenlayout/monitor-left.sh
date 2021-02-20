793dfe04f5d93f3eaa1c29c6322b10422bd56714
Jonathan Alves <alves_aj@windowslive.com>
2021-02-20 11:00:45 -0300

    added xinitrc and screenlayout

--- screenlayout/monitor-left.sh
+++ screenlayout/monitor-left.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI2 --mode 1360x768 --pos 0x0 --rotate normal --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1366x768 --pos 1360x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output HDMI2 --primary --mode 1360x768 --pos 1366x0 --rotate normal --output HDMI1 --off --output DP1 --off --output eDP1 --mode 1366x768 --pos 0x0 --rotate normal --output VIRTUAL1 --off
